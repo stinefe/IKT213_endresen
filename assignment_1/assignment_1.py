4f747e171099c2bb5990223ee57269336d076ad0
stinefe <stinefe@uia.no>
2026-08-23 20:28:02 +0200

    assignment_1: added camera and image information

---
+++ assignment_1/assignment_1.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy
+
+def print_image_information(image):
+    """prints the information about the image"""
+    height, width, channels = image.shape
+    print("height: ", height)
+    print("width: ", width)
+    print("channels: ", channels)
+    print("size: ", image.size)
+    print("data type: ", image.dtype)
+
+def save_camera_information():
+    """saves the camera information into a .txt file"""
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    fps = camera.get(cv2.CAP_PROP_FPS)
+    width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    with open ("solutions/camera_outputs.txt", "w") as f:
+        f.write(f"fps: {fps}\n")
+        f.write(f"height: {height}\n")
+        f.write(f"width: {width}\n")
+
+    camera.release()
+
+def main():
+    image = cv2.imread("iris-1.jpg")
+    print_image_information(image)
+    save_camera_information()
+
+if __name__ == '__main__':
+    main()
